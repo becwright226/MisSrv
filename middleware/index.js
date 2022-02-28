46b470825db5cba55fe3a7d16b6be841893f1fcc
becwright226 <becwright226@gmail.com>
2022-02-28 15:48:27 -0500

    trimmed model index, add valiSess to middle, update middleware index

--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,4 @@
-//code for index file
 module.exports = {
-    CORS: require('./headers')}
+    CORS: require('./headers'),
+    validateSession: require('./validate-session')
+}
--- model/index.js
+++ model/index.js
@@ -1,19 +1,19 @@
-const PostModel = require('./post');
-const UserModel = require('./user');
-const DiaryModel = require('./diary');
-const SchedModel = require('./schedule');
-const LogModel =require('./workLog');
-const RecipeModel =require('./recipe');
-const CommentModel =require('./comment');
-const OrderModel =require('./order');
+//const PostModel = require('./post');
+//const UserModel = require('./user');
+//const DiaryModel = require('./diary');
+//const SchedModel = require('./schedule');
+//const LogModel =require('./workLog');
+//const RecipeModel =require('./recipe');
+//const CommentModel =require('./comment');
+//const OrderModel =require('./order');
 
 module.exports = {
-    PostModel,
-    UserModel,
-    DiaryModel,
-    SchedModel,
-    LogModel,
-    RecipeModel,
-    CommentModel,
-    OrderModel
+    PostModel: require('./post'),
+    UserModel: require('./user'),
+    DiaryModel: require('./diary'),
+    SchedModel: require('./schedule'),
+    LogModel: require('./workLog'),
+    RecipeModel: require('./recipe'),
+    CommentModel: require('./comment'),
+    OrderModel: require('./order')
 }

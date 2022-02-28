4268b9d051cf78d83bea98d35f3f6dfcea615fdd
becwright226 <becwright226@gmail.com>
2022-02-28 15:35:23 -0500

    model index.js updated

--- model/index.js
+++ model/index.js
@@ -1,1 +1,19 @@
-//code for index file
+const PostModel = require('./post');
+const UserModel = require('./user');
+const DiaryModel = require('./diary');
+const SchedModel = require('./schedule');
+const LogModel =require('./workLog');
+const RecipeModel =require('./recipe');
+const CommentModel =require('./comment');
+const OrderModel =require('./order');
+
+module.exports = {
+    PostModel,
+    UserModel,
+    DiaryModel,
+    SchedModel,
+    LogModel,
+    RecipeModel,
+    CommentModel,
+    OrderModel
+}

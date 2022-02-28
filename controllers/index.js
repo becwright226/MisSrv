5c64962f674d0d5981742b0aa0ec7cfdcd8b76ec
becwright226 <becwright226@gmail.com>
2022-02-28 17:14:54 -0500

    controller files added, exported controllers through index

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,10 @@
-//code for index file
+module.exports = {
+    userController: require('./userController'),
+    postController: require('./postController'),
+    orderController: require('./orderController'),
+    recipeController: require('./recipeController'),
+    logController: require('./wLogController'),
+    schedController: require('./schedController'),
+    diaryController: require('./diaryController'),
+    commentController: require('./commentController') 
+  };
